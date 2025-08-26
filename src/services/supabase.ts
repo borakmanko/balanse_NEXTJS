2555ac7b11b937714c00e727515f0327fe75384a
Roi Regis <roiemerson.regis@cit.edu>
2025-08-26 12:49:10 +0800

    fix directory

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -8,7 +8,7 @@ export function useSupabase() {
   const [supabase] = useState(() => 
     createBrowserClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
     )
   )
 
